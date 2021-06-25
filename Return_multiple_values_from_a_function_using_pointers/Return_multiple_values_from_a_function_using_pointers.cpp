8a92fef5961e811d3620e3c1a05f9fabfec3340f
yovyTheLurker <yoerodri@yahoo.com>
2021-06-25 15:26:23 -0400

    Add project files.

---
+++ Return_multiple_values_from_a_function_using_pointers/Return_multiple_values_from_a_function_using_pointers.cpp
@@ -0,0 +1,50 @@
+// Return_multiple_values_from_a_function_using_pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int getMin(int numbers[], int size) {
+	int min=numbers[0];
+	for (int i = 1; i < 5; i++) {
+		if (numbers[i] < min) {
+			min = numbers[i];
+		}
+	}
+	return min;
+}
+int getMax(int numbers[], int size) {
+	int max = numbers[0];
+	for (int i = 1; i < 5; i++) {
+		if (numbers[i] > max) {
+			max = numbers[i];
+		}
+	}
+	return max;
+}
+void getMinAndMax(int numbers[], int size, int* max, int* min) {
+	for (int i = 1; i < 5; i++) {
+		if (numbers[i] > *max) {
+			*max = numbers[i];
+		}
+		if (numbers[i] < *min) {
+			*min = numbers[i];
+		}
+	}
+}
+
+int main()
+{
+	int numbers[5] = { 1,2,3,4,5 };
+	//cout << "Min is " << getMin(numbers, 5) << endl;
+	//cout << "Max is " << getMax(numbers, 5);
+	int min = numbers[0];
+	int max = numbers[0];
+	getMinAndMax(numbers, 5, &min, &max);
+	cout << "Min is " << min << endl;
+	cout << "Max is " << max << endl;
+
+
+	system("pause>0");
+}
+
+		
